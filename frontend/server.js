9a50b7420e4e6807e53191cc753eac7bd567dcac
Teja Siddhartha Rajam <73954357+rtsiddhartha@users.noreply.github.com>
2021-12-01 04:06:34 -0500

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -5,10 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('__dirname + dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+//     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+          const fullPath = path.join(__dirname + '/dist/angular-app-heroku/index.html');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+console.log('Server started running..');
 );
 
 // Start the app by listening on the default Heroku port
