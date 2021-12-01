f94069b8d4795e5b0a976f8e44d2d7b2e5d1fb8d
Teja Siddhartha Rajam <73954357+rtsiddhartha@users.noreply.github.com>
2021-12-01 04:15:40 -0500

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -4,16 +4,31 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('__dirname + dist/angular-app-heroku'));
+// // Serve only the static files form the dist directory
+// app.use(express.static('__dirname + dist/angular-app-heroku'));
 
-app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-          const fullPath = path.join(__dirname + '/dist/angular-app-heroku/index.html');
+// app.get('/*', (req, res) =>
+// //     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+//           const fullPath = path.join(__dirname + '/dist/angular-app-heroku/index.html');
+//   console.log(" Fetching from.." + fullPath);
+//     res.sendFile(fullPath);
+// console.log('Server started running..');
+// );
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT ||3000);
+
+app.use(express.static(__dirname + '/dist/angular-app-heroku'));
+app.listen(process.env.PORT || 3000);
+
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/angular-app-heroku/index.html');
   console.log(" Fetching from.." + fullPath);
     res.sendFile(fullPath);
-console.log('Server started running..');
-);
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT ||3000);
+console.log('Server started running..');
+
+//Changed to run on Heroku
